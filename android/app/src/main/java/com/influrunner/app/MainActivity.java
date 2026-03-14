637faf109c61685db6b865305151d6dbe1a30b0c
KrishThakkar2205 <kdthakkar22@gmail.com>
2026-03-14 12:23:02 +0530

    feat: add app icon, splash screen and Capacitor Android/iOS native projects

---
+++ android/app/src/main/java/com/influrunner/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.influrunner.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Required for Capacitor: assets must use relative paths (file:// WebView)
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
 })
